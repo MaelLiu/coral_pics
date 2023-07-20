221f1266b2c59c5326822f735dfc7625ad76650b
Mael Liu <owen920614@gmail.com>
2023-07-20 21:58:22 +0800

    add a new image

--- rename.py
+++ rename.py
@@ -1,5 +1,10 @@
 import os
 for file in os.listdir("."):
     if '.' not in file:
-        os.rename(file, file+".png")
-        print(file + " is renamed")
+        new_file = file + ".png"
+        os.rename(file, new_file)
+        os.system("git add .")
+        os.system("git commit -m 'add a new image'")
+        os.system("git push origin main")
+        os.system("rm {new_file}")
+
