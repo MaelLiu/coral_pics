bd6db0c0a25f5f10a5bc3be88f67847df93a39d4
Mael Liu <owen920614@gmail.com>
2023-07-20 22:06:35 +0800

    reset

--- rename.py
+++ rename.py
@@ -3,9 +3,7 @@ for file in os.listdir("."):
     if '.' not in file:
         new_file = file + ".png"
         os.rename(file, new_file)
-        os.system("git add .")
-        os.system("git commit -m 'add a new image'")
-        os.system("git push origin main")
-        print(new_file)
-        os.system("rm " + new_file)
+os.system("git add .")
+os.system("git commit -m 'add a new image'")
+os.system("git push origin main")
 
